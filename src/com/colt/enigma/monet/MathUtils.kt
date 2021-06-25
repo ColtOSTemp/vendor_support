0159137bff7676ae445f629d8007e32a65eb6109
Maitreya29 <maitreyapatni30@gmail.com>
2021-06-25 14:48:37 +0530

    Support: Introduce Monet
    Co-authored-by: Kyant2021 <kyant2021@outlook.com>
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

---
+++ src/com/colt/enigma/monet/MathUtils.kt
@@ -0,0 +1,7 @@
+package com.colt.enigma.monet
+
+public object MathUtils {
+    public fun lerp(start: Double, stop: Double, amount: Double): Double {
+        return (stop - start) * amount + start
+    }
+}
